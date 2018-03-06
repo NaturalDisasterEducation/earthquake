9f90224d0af2a7f5d1b1ed68be15c37291f20fd4
Denise Ho <deniseho630@gmail.com>
2018-03-06 22:02:42 +0000

    rename and integrate pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,13 @@
 // vertical menu home icon
 $("#fp-nav").prepend(`
 <div class="clearfix">
-<h5 class="l-right">
+<h5 class="l-right" id="GoNDEHome">
     <a href="earth.html">
         <span class="fui-home"></span>
     </a>
 </h5>
 </div>`);
+
+$("#GoNDEHome").on("click", function(){
+    window.location.href = "earth.html";
+})
