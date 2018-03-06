ed217b952859f900f03fff2c11d1ab2c4ae61017
Denise Ho <deniseho630@gmail.com>
2018-03-06 18:53:37 +0000

    edit vertical menu

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+// vertical menu home icon
+$("#fp-nav").prepend(`
+<div class="clearfix">
+<h5 class="l-right">
+    <a href="earth.html">
+        <span class="fui-home"></span>
+    </a>
+</h5>
+</div>`);
--- assets/js/fullpage.js
+++ assets/js/fullpage.js
@@ -1,67 +1,65 @@
-
 // variables
 var $header_top = $('.header-top');
 var $nav = $('nav');
 
-
-
-// toggle menu 
-$header_top.find('a').on('click', function() {
-  $(this).parent().toggleClass('open-menu');
-});
-
-
+// toggle menu
+$header_top
+  .find('a')
+  .on('click', function () {
+    $(this)
+      .parent()
+      .toggleClass('open-menu');
+  });
 
 // fullpage customization
 $('#fullpage').fullpage({
-  sectionsColor: ['#ffffff', '#f1f1f1', '#ffffff', '#f1f1f1', '#ffffff'],
+  sectionsColor: [
+    '#ffffff', '#f1f1f1', '#ffffff', '#f1f1f1', '#ffffff'
+  ],
   sectionSelector: '.vertical-scrolling',
   slideSelector: '.horizontal-scrolling',
   navigation: true,
-  navigationTooltips:  ['Earthquake', 'Before', 'During', 'After'],
+  navigationTooltips: [
+    'Earthquake', 'Before', 'During', 'After'
+  ],
   showActiveTooltip: true,
   slidesNavigation: true,
   controlArrows: false,
-  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection'],
-  menu: '#menu',
+  anchors: [
+    'firstSection', 'secondSection', 'thirdSection', 'fourthSection'
+  ],
+  menu: false,
 
-  afterLoad: function(anchorLink, index) {
+  //Accessibility
+  keyboardScrolling: true,
+  animateAnchor: true,
+  recordHistory: true,
+
+  afterLoad: function (anchorLink, index) {
     // $header_top.css('background', 'rgba(0, 47, 77, .3)');
     $nav.css('background', 'rgba(0, 47, 77, .25)');
-    // if (index == 1) {
-    //     $('#fp-nav').hide();
-    //   }
+    // if (index == 1) {     $('#fp-nav').hide();   }
   },
 
-  onLeave: function(index, nextIndex, direction) {
-    if(index != 0) {
+  onLeave: function (index, nextIndex, direction) {
+    if (index != 0) {
       $('#fp-nav').show();
     }
   },
 
-  afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
+  afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
     // if(anchorLink == 'fifthSection' && slideIndex == 1) {
-    //   $.fn.fullpage.setAllowScrolling(false, 'up');
-    //   $header_top.css('background', 'transparent');
-    //   $nav.css('background', 'transparent');
-    //   $(this).css('background', '#374140');
-    //   $(this).find('h2').css('color', 'white');
-    //   $(this).find('h3').css('color', 'white');
-    //   $(this).find('p').css(
-    //     {
-    //       'color': '#DC3522',
-    //       'opacity': 1,
-    //       'transform': 'translateY(0)'
-    //     }
-    //   );
-    // }
+    // $.fn.fullpage.setAllowScrolling(false, 'up');   $header_top.css('background',
+    // 'transparent');   $nav.css('background', 'transparent');
+    // $(this).css('background', '#374140');   $(this).find('h2').css('color',
+    // 'white');   $(this).find('h3').css('color', 'white');
+    // $(this).find('p').css(     {       'color': '#DC3522',       'opacity': 1,
+    //    'transform': 'translateY(0)'     }   ); }
   },
 
-  onSlideLeave: function( anchorLink, index, slideIndex, direction) {
+  onSlideLeave: function (anchorLink, index, slideIndex, direction) {
     // if(anchorLink == 'fifthSection' && slideIndex == 1) {
-    //   $.fn.fullpage.setAllowScrolling(true, 'up');
-      // $header_top.css('background', 'rgba(0, 47, 77, .3)');
-      // $nav.css('background', 'rgba(0, 47, 77, .25)');
-    // }
-  } 
-}); 
+    // $.fn.fullpage.setAllowScrolling(true, 'up'); $header_top.css('background',
+    // 'rgba(0, 47, 77, .3)'); $nav.css('background', 'rgba(0, 47, 77, .25)'); }
+  }
+});
