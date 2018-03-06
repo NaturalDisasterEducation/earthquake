35e33cd28a8a9fd6f0859bc4320653acb7cc1c1f
Denise Ho <deniseho630@gmail.com>
2018-03-06 16:52:07 +0000

    change vertical menu style

--- assets/js/fullpage.js
+++ assets/js/fullpage.js
@@ -18,6 +18,8 @@ $('#fullpage').fullpage({
   sectionSelector: '.vertical-scrolling',
   slideSelector: '.horizontal-scrolling',
   navigation: true,
+  navigationTooltips:  ['Earthquake', 'Before', 'During', 'After'],
+  showActiveTooltip: true,
   slidesNavigation: true,
   controlArrows: false,
   anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection'],
@@ -26,9 +28,9 @@ $('#fullpage').fullpage({
   afterLoad: function(anchorLink, index) {
     // $header_top.css('background', 'rgba(0, 47, 77, .3)');
     $nav.css('background', 'rgba(0, 47, 77, .25)');
-    if (index == 1) {
-        $('#fp-nav').hide();
-      }
+    // if (index == 1) {
+    //     $('#fp-nav').hide();
+    //   }
   },
 
   onLeave: function(index, nextIndex, direction) {
@@ -38,21 +40,21 @@ $('#fullpage').fullpage({
   },
 
   afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
-    if(anchorLink == 'fifthSection' && slideIndex == 1) {
-      $.fn.fullpage.setAllowScrolling(false, 'up');
-      $header_top.css('background', 'transparent');
-      $nav.css('background', 'transparent');
-      $(this).css('background', '#374140');
-      $(this).find('h2').css('color', 'white');
-      $(this).find('h3').css('color', 'white');
-      $(this).find('p').css(
-        {
-          'color': '#DC3522',
-          'opacity': 1,
-          'transform': 'translateY(0)'
-        }
-      );
-    }
+    // if(anchorLink == 'fifthSection' && slideIndex == 1) {
+    //   $.fn.fullpage.setAllowScrolling(false, 'up');
+    //   $header_top.css('background', 'transparent');
+    //   $nav.css('background', 'transparent');
+    //   $(this).css('background', '#374140');
+    //   $(this).find('h2').css('color', 'white');
+    //   $(this).find('h3').css('color', 'white');
+    //   $(this).find('p').css(
+    //     {
+    //       'color': '#DC3522',
+    //       'opacity': 1,
+    //       'transform': 'translateY(0)'
+    //     }
+    //   );
+    // }
   },
 
   onSlideLeave: function( anchorLink, index, slideIndex, direction) {
