c48359bdc43fed3895faa62cb50faec9516174ba
Denise Ho <deniseho630@gmail.com>
2018-03-07 18:02:25 +0000

    add collapse footer in NDE homepage

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,6 +8,19 @@ $("#fp-nav").prepend(`
 </h5>
 </div>`);
 
-$("#GoNDEHome").on("click", function(){
+$("#GoNDEHome").on("click", function () {
     window.location.href = "earth.html";
+})
+
+// natural disaster homepage footer collapse
+$('#FooterContent').on('show.bs.collapse', function () {
+    $('.panel-heading .glyphicon.glyphicon-chevron-up').css({
+        'transform': 'rotate(180deg)'
+    }, 500);
+})
+
+$('#FooterContent').on('hide.bs.collapse', function () {
+    $('.panel-heading .glyphicon.glyphicon-chevron-up').css({
+        'transform': 'rotate(0deg)'
+    }, 500);
 })
