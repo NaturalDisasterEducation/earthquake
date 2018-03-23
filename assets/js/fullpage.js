3ae9e75d4b3dbf592645d04daa4a1b61a319a981
Denise Ho <deniseho630@gmail.com>
2018-03-23 20:24:03 +0000

    arrange fullpage autoscrolling codes

--- assets/js/fullpage.js
+++ assets/js/fullpage.js
@@ -1,14 +1,14 @@
 $('.btn[data-toggle=modal]').on('click', function () {
   $.fn.fullpage.destroy('all');
-  fullpageNotAutoScroll();
+  fullpage(false);
 });
 
 $("btn.infoPage_home").on("click", function(){
   $.fn.fullpage.destroy('all');
-  fullpageAutoScroll();
+  fullpage(true);
 })
 
-function fullpageAutoScroll(){
+function fullpage(autoScroll){
   $('#fullpage').fullpage({
     // sectionsColor: [   '#ffffff', '#f1f1f1', '#ffffff', '#f1f1f1', '#ffffff' ],
     // sectionSelector: '.vertical-scrolling', slideSelector:
@@ -16,11 +16,11 @@ function fullpageAutoScroll(){
     // class="fui-home"></span>`, 'Before', 'During', 'After' ],
     // showActiveTooltip:// true,
     slidesNavigation: true,
-    autoScrolling: true,
+    autoScrolling: autoScroll,
     scrollBar: true,
     fitToSectionDelay: 0,
     scrollOverflowReset: true,
-    // scrollingSpeed: 1000, css3: false,
+    // scrollingSpeed: 1000, 
     anchors: [
       'firstSection', 'secondSection', 'thirdSection', 'fourthSection'
     ],
@@ -48,47 +48,8 @@ function fullpageAutoScroll(){
   });
 }
 
-function fullpageNotAutoScroll(){
-  $('#fullpage').fullpage({
-    // sectionsColor: [   '#ffffff', '#f1f1f1', '#ffffff', '#f1f1f1', '#ffffff' ],
-    // sectionSelector: '.vertical-scrolling', slideSelector:
-    // '.horizontal-scrolling', navigation: true, navigationTooltips: [   `<span//
-    // class="fui-home"></span>`, 'Before', 'During', 'After' ],
-    // showActiveTooltip:// true,
-    slidesNavigation: true,
-    autoScrolling: false,
-    scrollBar: true,
-    fitToSectionDelay: 0,
-    scrollOverflowReset: true,
-    // scrollingSpeed: 1000, css3: false,
-    anchors: [
-      'firstSection', 'secondSection', 'thirdSection', 'fourthSection'
-    ],
-    menu: '#menu',
-  
-    //Accessibility
-    keyboardScrolling: true,
-    animateAnchor: true,
-    recordHistory: true,
-  
-    afterRender: function () {},
-  
-    afterLoad: function (anchorLink, index) {
-      $(".section").css({"visibility": "visible"});
-    },
-  
-    onLeave: function (index, nextIndex, direction) {
-      // if (index != 0) {   $('#fp-nav').show(); }
-    },
-  
-    afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {},
-  
-    onSlideLeave: function (anchorLink, index, slideIndex, direction) {}
-  });
-}
-
 $('document').ready(function () {
-  fullpageAutoScroll();
+  fullpage(true);
   skrollr.init({
     smoothScrolling: true,
     forceHeight: true,
