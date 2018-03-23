c76a000f0aa320f794d86881cac0d64b52839357
Denise Ho <deniseho630@gmail.com>
2018-03-23 17:33:15 +0000

    add lazyload

--- assets/js/fullpage.js
+++ assets/js/fullpage.js
@@ -1,6 +1,3 @@
-// // variables var $header_top = $('.header-top'); var $nav = $('nav'); //
-// toggle menu $header_top   .find('a')   .on('click', function () {     $(this)
-//       .parent()       .toggleClass('open-menu');   }); fullpage customization
 $('#fullpage').fullpage({
   // sectionsColor: [   '#ffffff', '#f1f1f1', '#ffffff', '#f1f1f1', '#ffffff' ],
   // sectionSelector: '.vertical-scrolling', slideSelector:
@@ -8,13 +5,10 @@ $('#fullpage').fullpage({
   // class="fui-home"></span>`, 'Before', 'During', 'After' ],
   // showActiveTooltip:// true,
   slidesNavigation: true,
-  scrollOverflow: true,
-  autoScrolling: true,
-  scrollOverflowReset: true,
-  // controlArrows: false,
+  autoScrolling: false,
   scrollBar: true,
-  scrollingSpeed: 1000,
-  // css3: false,
+  // scrollOverflowReset: true, controlArrows: false, scrollBar: true,
+  // scrollingSpeed: 1000, css3: false,
   anchors: [
     'firstSection', 'secondSection', 'thirdSection', 'fourthSection'
   ],
@@ -25,9 +19,12 @@ $('#fullpage').fullpage({
   animateAnchor: true,
   recordHistory: true,
 
+  afterRender: function () {
+
+  },
+
   afterLoad: function (anchorLink, index) {
-    // $header_top.css('background', 'rgba(0, 47, 77, .3)'); $nav.css('background',
-    // 'rgba(0, 47, 77, .25)'); if (index == 1) {     $('#fp-nav').hide();   }
+    $(".section").css({"visibility": "visible"});
   },
 
   onLeave: function (index, nextIndex, direction) {
@@ -35,18 +32,22 @@ $('#fullpage').fullpage({
   },
 
   afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-    // if(anchorLink == 'fifthSection' && slideIndex == 1) {
-    // $.fn.fullpage.setAllowScrolling(false, 'up');   $header_top.css('background',
-    // 'transparent');   $nav.css('background', 'transparent');
-    // $(this).css('background', '#374140');   $(this).find('h2').css('color',
-    // 'white');   $(this).find('h3').css('color', 'white'); $(this).find('p').css(
-    //   {       'color': '#DC3522',       'opacity': 1,    'transform':
-    // 'translateY(0)'     }   ); }
+
   },
 
-  onSlideLeave: function (anchorLink, index, slideIndex, direction) {
-    // if(anchorLink == 'fifthSection' && slideIndex == 1) {
-    // $.fn.fullpage.setAllowScrolling(true, 'up'); $header_top.css('background',
-    // 'rgba(0, 47, 77, .3)'); $nav.css('background', 'rgba(0, 47, 77, .25)'); }
-  }
+  onSlideLeave: function (anchorLink, index, slideIndex, direction) {}
+});
+
+
+$('document').ready(function(){
+  skrollr.init({
+    smoothScrolling: true,
+    forceHeight: true,
+    mobileCheck: function () {
+        if ((/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+            // mobile device
+        }
+    }
+});
+  bindScroll();
 });
