23e54189e1fe19252d3c4dc653417af286a84034
Killian Desmond <killian.desmond@gmail.com>
2018-03-29 00:46:08 +0100

    added javascript and new style to an updated version of the before3 html page

---
+++ assets/js/backpack.js
@@ -0,0 +1,50 @@
+$('.blanket').on('click', function(){
+	$(this).toggleClass('running');
+	$('.blanketref').toggleClass('on');
+});
+
+
+$('.medicine').on('click', function(){
+	$(this).toggleClass('running');
+	$('.medicineref').toggleClass('on');
+});
+
+$('.shoes').on('click', function(){
+	$(this).toggleClass('running');
+	$('.shoesref').toggleClass('on');
+});
+
+$('.toy').on('click', function(){
+	$(this).toggleClass('running');
+	$('.toyref').toggleClass('on');
+});
+
+$('.water').on('click', function(){
+	$(this).toggleClass('running');
+	$('.waterref').toggleClass('on');
+});
+
+$('.id').on('click', function(){
+	$(this).toggleClass('running');
+	$('.idref').toggleClass('on');
+});
+
+$('.map').on('click', function(){
+	$(this).toggleClass('running');
+	$('.mapref').toggleClass('on');
+});
+
+$('.mask').on('click', function(){
+	$(this).toggleClass('running');
+	$('.maskref').toggleClass('on');
+});
+
+$('.food').on('click', function(){
+	$(this).toggleClass('running');
+	$('.foodref').toggleClass('on');
+});
+
+$('.firstaid').on('click', function(){
+	$(this).toggleClass('running');
+	$('.firstaidref').toggleClass('on');
+});
