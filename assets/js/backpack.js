d5fc1210b5709a7cccbe4f08a9d8d6c75c4e2201
Killian Desmond <killian.desmond@gmail.com>
2018-03-29 01:07:56 +0100

    changed bag animation

--- assets/js/backpack.js
+++ assets/js/backpack.js
@@ -1,50 +1,61 @@
 $('.blanket').on('click', function(){
 	$(this).toggleClass('running');
 	$('.blanketref').toggleClass('on');
+	$('.backpackImg').toggleClass('fill');
 });
 
 
 $('.medicine').on('click', function(){
 	$(this).toggleClass('running');
 	$('.medicineref').toggleClass('on');
+	$('.backpackImg').toggleClass('fill');
 });
 
 $('.shoes').on('click', function(){
 	$(this).toggleClass('running');
 	$('.shoesref').toggleClass('on');
+	$('.backpackImg').toggleClass('fill');
 });
 
 $('.toy').on('click', function(){
 	$(this).toggleClass('running');
 	$('.toyref').toggleClass('on');
+	$('.backpackImg').toggleClass('fill');
 });
 
 $('.water').on('click', function(){
 	$(this).toggleClass('running');
 	$('.waterref').toggleClass('on');
+	$('.backpackImg').toggleClass('fill');
 });
 
 $('.id').on('click', function(){
 	$(this).toggleClass('running');
 	$('.idref').toggleClass('on');
+	$('.backpackImg').toggleClass('fill');
 });
 
 $('.map').on('click', function(){
 	$(this).toggleClass('running');
 	$('.mapref').toggleClass('on');
+	$('.backpackImg').toggleClass('fill');
 });
 
 $('.mask').on('click', function(){
 	$(this).toggleClass('running');
 	$('.maskref').toggleClass('on');
+	$('.backpackImg').toggleClass('fill');
 });
 
 $('.food').on('click', function(){
 	$(this).toggleClass('running');
 	$('.foodref').toggleClass('on');
+	$('.backpackImg').toggleClass('fill');
 });
 
 $('.firstaid').on('click', function(){
 	$(this).toggleClass('running');
 	$('.firstaidref').toggleClass('on');
+	$('.backpackImg').toggleClass('fill');
 });
+
